66aee5d199005bfa5f4344381acaf3af848ee86c
Andrey Sprynga <rtx4d@aospa.co>
2026-04-17 23:28:01 +0700

    trident: Add firmware extraction support

--- Android.mk
+++ Android.mk
@@ -3,3 +3,23 @@
 #
 
 LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),trident)
+
+$(call add-radio-file-sha1-checked,radio/abl.img,a73368fd5d1a1e2eaf5dbf62d5f8cc5951cf6182)
+$(call add-radio-file-sha1-checked,radio/aop.img,1bd8a80217390791671bad5d42e2072a56df8cfa)
+$(call add-radio-file-sha1-checked,radio/bluetooth.img,4966264c3935b9cb0b6af80b16055176c89638e2)
+$(call add-radio-file-sha1-checked,radio/cmnlib.img,e02eb3001f913557c0723ec3e0a5e0c27fa79b65)
+$(call add-radio-file-sha1-checked,radio/cmnlib64.img,2eb35992f55200ec0e4342e83c3ebc749a8ba790)
+$(call add-radio-file-sha1-checked,radio/devcfg.img,147431a218cc0878bfd5f38f81b23cebbb769e6c)
+$(call add-radio-file-sha1-checked,radio/dsp.img,6bccbf5f57954dc8e06f2ba660b567e96fdb481e)
+$(call add-radio-file-sha1-checked,radio/hyp.img,fcceeb751420b779e2b47c6783d4cb2675647bb3)
+$(call add-radio-file-sha1-checked,radio/keymaster.img,0886655727354da38f771185b4f9b58710fc802a)
+$(call add-radio-file-sha1-checked,radio/modem.img,3fe0fa712d6d893ba570c49de89a95485c987657)
+$(call add-radio-file-sha1-checked,radio/qupfw.img,24842b6682059c65ff70cfb35e07ff9056905c3d)
+$(call add-radio-file-sha1-checked,radio/storsec.img,9ef81c47f60013bca7694f48d2778c93f48e8960)
+$(call add-radio-file-sha1-checked,radio/tz.img,bbc21fad792761431ec21d276ed119caaa003445)
+$(call add-radio-file-sha1-checked,radio/xbl.img,d79c8abed91902ce825494938ccc6e66b651eb21)
+$(call add-radio-file-sha1-checked,radio/xbl_config.img,f1a4095035f3dd2fd8a25192ee5441e2e3bc2ae9)
+
+endif
--- BoardConfigVendor.mk
+++ BoardConfigVendor.mk
@@ -1,3 +1,20 @@
 #
 # Automatically generated file. DO NOT MODIFY
 #
+
+AB_OTA_PARTITIONS += \
+    abl \
+    aop \
+    bluetooth \
+    cmnlib \
+    cmnlib64 \
+    devcfg \
+    dsp \
+    hyp \
+    keymaster \
+    modem \
+    qupfw \
+    storsec \
+    tz \
+    xbl \
+    xbl_config
